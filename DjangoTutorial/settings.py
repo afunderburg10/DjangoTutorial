18aad5dcfd5df2cc566921aadcdf0092207c2577
afunderburg <afunderburg@connectwise.com>
2017-05-19 16:01:50 -0400

    Move static files Add some dummy menu content

--- DjangoTutorial/settings.py
+++ DjangoTutorial/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 TEMPLATE_LOADERS = (
     'django_jinja.loaders.AppLoader',
     'django_jinja.loaders.FileSystemLoader',
