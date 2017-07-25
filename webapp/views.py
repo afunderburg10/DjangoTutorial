fe1deed8d0e194a68c21af4bd53fce6770449eb7
afunderburg10 <afunderburg10@gmail.com>
2017-07-24 21:48:03 -0400

    Add detail and results html pages and setup views for them

--- webapp/views.py
+++ webapp/views.py
@@ -20,14 +20,20 @@ def polls(request):
 
 def polls_detail(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return HttpResponse('<h1>Polls Detail Page</h1><h2>Question ID: {0}</h2>'.format(question_id))
-    # return render(request, 'webapp/polls.html')
+    context = {
+        'question': question
+    }
+    return render(request, 'webapp/polls_detail.html', context=context)
 
 
 def polls_results(request, question_id):
-    return HttpResponse('<h1>Polls Results Page</h1><h2>Question ID: {0}</h2>'.format(question_id))
-    # return render(request, 'webapp/polls.html')
+    question = get_object_or_404(Question, pk=question_id)
+    choices = question.choice_set.all()
+    context = {
+        'question': question,
+        'choices': choices
+    }
+    return render(request, 'webapp/polls_results.html', context=context)
 
 
 def polls_vote(request, question_id):
