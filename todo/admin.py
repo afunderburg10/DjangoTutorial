4e5ea88325dc47fa14b15af6d803270a7e89bc49
afunderburg10 <afunderburg10@gmail.com>
2017-08-14 21:04:08 -0400

    Created ToDoItem model Created ToDoStatus model Created migrations for both models. Ran migrations on dev database

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from todo.models import ToDoStatus, ToDoItem
+
+admin.site.register(ToDoStatus)
+admin.site.register(ToDoItem)
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDoStatus(models.Model):
+    text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.text
+
+
+class ToDoItem(models.Model):
+    sort_order = models.BigIntegerField(unique=True)
+    status = models.ForeignKey(ToDoStatus)
+    text = models.CharField(max_length=500)
+    create_date = models.DateTimeField('date created')
+    due_date = models.DateTimeField('date due')
+
+    def __str__(self):
+        return self.text
